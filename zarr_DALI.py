5adc84f4938cc7f8a157cb114190340cd1b80809
Negin Sobhani <negin513@gmail.com>
2025-02-24 17:47:38 -0700

    updates to zarr dali

--- zarr_DALI.py
+++ zarr_DALI.py
@@ -17,6 +17,7 @@ def create_dali_pipeline(dataset, batch_size, device='gpu'):
         batch_size (int): Batch size for the pipeline.
         device (str): Device to use ('gpu' or 'cpu'). Defaults to 'gpu'.
     """
+    
     # Define external source to fetch data from the dataset
     inputs, targets = fn.external_source(
         source=dataset,
@@ -38,13 +39,15 @@ def create_dali_pipeline(dataset, batch_size, device='gpu'):
 if __name__ == "__main__":
     ## Example usage of the ERA5TimeSeriesDataset class
     data_path = "/glade/derecho/scratch/ksha/CREDIT_data/ERA5_mlevel_arXiv"
-    start_year = 2000
+    start_year = 2001
     end_year = 2010
 
     # for now, just surface variables!cd
     input_vars = ['t2m', 'V500', 'U500', 'T500', 'Z500', 'Q500']
     target_vars = ['t2m']
 
+    # Enable GPU support
+    zarr.config.enable_gpu()
 
     #dataset = ERA5TimeSeriesDataset(data_path, start_year, end_year, input_vars=input_vars)
 
@@ -60,20 +63,31 @@ if __name__ == "__main__":
 
     print(era5_dataset)       
     print(f"Total samples: {len(era5_dataset)}")
+
     #pytorch_dataset = PyTorchERA5Dataset(train_dataset)
 
 
     # Create DALI pipeline
     batch_size = 32
-    pipe = create_dali_pipeline(era5_dataset.fetch_timeseries(), batch_size=batch_size, device='gpu', num_threads=4, device_id=0)
+
+    #pipe = create_dali_pipeline(era5_dataset.fetch_timeseries(), batch_size=batch_size, device='gpu', num_threads=4, device_id=0)
+    #pipe.build() # This fails here!
+
+    pipe = Pipeline(batch_size=batch_size, num_threads=2, device_id=0)
+    with pipe:
+        inputs, targets = create_dali_pipeline(era5_dataset.fetch_timeseries(), batch_size=batch_size, device='gpu')
+        pipe.set_outputs(inputs, targets)
+
+
     pipe.build()
+    pipe_out = pipe.run()    
 
     # Create DALI iterator
-    dali_iter = DALIGenericIterator(pipe, output_map=["inputs", "targets"], size=len(era5_dataset))
+    #dali_iter = DALIGenericIterator(pipe, output_map=["inputs", "targets"], size=len(era5_dataset))
 
     # Fetch a batch of data
-    for batch in dali_iter:
-        inputs = batch[0]["inputs"]
-        targets = batch[0]["targets"]
-        print(f"Input shape: {inputs.shape}, Target shape: {targets.shape}")
-        break # just a test
+    #for batch in dali_iter:
+    #    inputs = batch[0]["inputs"]
+    #    targets = batch[0]["targets"]
+    #    print(f"Input shape: {inputs.shape}, Target shape: {targets.shape}")
+    #    break # just a test
