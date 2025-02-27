b673bb31023321b10e92d7fa3a7adf1960ad0fc3
Max Jones <maxjones@casper04.hpc.ucar.edu>
2025-02-27 13:28:50 -0700

    Update results

--- bash_scripts/create_array.py
+++ bash_scripts/create_array.py
@@ -1,4 +1,10 @@
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser("create_file")
+parser.add_argument('filename')
+args = parser.parse_args()
 
 array = np.random.rand(10**8, 12)
-np.save('/glade/derecho/scratch/maxjones/tmp/large_array.npy', array)
+
+np.save(args.filename, array)
